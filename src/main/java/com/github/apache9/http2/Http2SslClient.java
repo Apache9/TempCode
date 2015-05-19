ee32e41e6b346ab60db2c18ff4133f002fe7b3dc
zhangduo <palomino219@gmail.com>
2015-05-19 15:25:14 +0800

    MOD: https

--- src/main/java/com/github/apache9/http2/Http2SslClient.java
+++ src/main/java/com/github/apache9/http2/Http2SslClient.java
@@ -12,7 +12,18 @@ import io.netty.handler.codec.http.DefaultFullHttpRequest;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http2.Http2OrHttpChooser.SelectedProtocol;
+import io.netty.handler.codec.http2.Http2SecurityUtil;
 import io.netty.handler.codec.http2.HttpUtil;
+import io.netty.handler.ssl.ApplicationProtocolConfig;
+import io.netty.handler.ssl.ApplicationProtocolConfig.Protocol;
+import io.netty.handler.ssl.ApplicationProtocolConfig.SelectedListenerFailureBehavior;
+import io.netty.handler.ssl.ApplicationProtocolConfig.SelectorFailureBehavior;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslProvider;
+import io.netty.handler.ssl.SupportedCipherSuiteFilter;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
 import java.net.URI;
 import java.util.concurrent.TimeUnit;
@@ -20,14 +31,37 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author zhangduo
  */
-public class Http2Client {
+public class Http2SslClient {
 
     public static void main(String[] args) throws Exception {
         String addr = args[0];
         String[] hostAndPort = addr.split(":");
+
+        final SslContext sslCtx = SslContextBuilder
+                .forClient()
+                .sslProvider(SslProvider.OPENSSL)
+                /*
+                 * NOTE: the cipher filter may not include all ciphers required
+                 * by the HTTP/2 specification. Please refer to the HTTP/2
+                 * specification for cipher requirements.
+                 */
+                .ciphers(Http2SecurityUtil.CIPHERS, SupportedCipherSuiteFilter.INSTANCE)
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .applicationProtocolConfig(
+                        new ApplicationProtocolConfig(Protocol.ALPN,
+                        // NO_ADVERTISE is currently the only mode supported by
+                        // both OpenSsl and JDK providers.
+                                SelectorFailureBehavior.NO_ADVERTISE,
+                                // ACCEPT is currently the only mode supported
+                                // by both OpenSsl and JDK providers.
+                                SelectedListenerFailureBehavior.ACCEPT, SelectedProtocol.HTTP_2
+                                        .protocolName(), SelectedProtocol.HTTP_1_1.protocolName()))
+                .build();
+
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
-            Http2ClientInitializer initializer = new Http2ClientInitializer();
+            Http2SslClientInitializer initializer = new Http2SslClientInitializer(sslCtx);
+
             Bootstrap bootstrap = new Bootstrap().group(workerGroup)
                     .channel(NioSocketChannel.class)
                     .remoteAddress(hostAndPort[0], Integer.parseInt(hostAndPort[1]))
